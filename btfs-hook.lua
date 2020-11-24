4dea6dc1e559fb79037f977e81eb8060c783a1a3
ait <ait@Arch>
2020-11-24 19:17:16 +0100

    fixed detecting if folder was already mounted

--- btfs-hook.lua
+++ btfs-hook.lua
@@ -1,7 +1,7 @@
 -- mpv script to play torrents in mpv (https://github.com/aitet/mpv-btfs-hook)
 
 local settings = {
-   download_directory = "/tmp/btfs/",
+   download_directory = "/tmp/btfs",
 }
 local open_videos = {}
 
@@ -11,7 +11,7 @@ local function ends_with(str, ending)
 end
 
 local function dirLookup(dir)
-   local p = io.popen('find "'..dir..'" -type f')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.
+   local p = io.popen('find "'..dir..'" -type f | head -n 1')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.
    for file in p:lines() do                         --Loop through all files
        return file
    end
@@ -23,26 +23,28 @@ function play_torrent()
         -- find if the url is a torrent or magnet link
         if (url:find("magnet:") == 1  or ends_with(url, "torrent")) then
                 os.execute("mkdir -p " .. settings.download_directory)
-		local is_empty = dirLookup(settings.download_directory)
-		if is_empty then
+
+        	mp.msg.verbose("Checks /etc/mtab if any btfs filesystems are mounted")
+		local check_empty = os.execute("grep '^btfs' /etc/mtab 2>&1 >/dev/null")
+		if check_empty == true then
+        		mp.msg.verbose("Found mounted btfs")
         		os.execute("fusermount -u " .. settings.download_directory)
 		end
-                -- Mount the torrent with btfs and exit if it can't execute the command
+                -- Mount the torrent with btfs
         	mp.msg.verbose("Mouting directory:" .. settings.download_directory)
                 os.execute("btfs " .. url .. " " .. settings.download_directory)
 
-		--
-		local path = dirLookup(settings.download_directory)
                 -- get the filename in the mounted dir
+		local path = dirLookup(settings.download_directory)
+
+		open_videos[url] = {url=url}
                 mp.set_property("stream-open-filename", path)
         end
 end
 
 function torrent_cleanup()
-        local url = mp.get_property("stream-open-filename")
-
-        -- find if the url is a torrent or magnet link
-        if (url:find("magnet:") == 1  or ends_with(url, "torrent")) then
+	local url = mp.get_property("stream-open-filename")
+        if open_videos[url] then
         	mp.msg.verbose("Unmouting directory:" .. settings.download_directory)
         	os.execute("fusermount -u " .. settings.download_directory)
       		open_videos[url] = {}
